9b0b7bfc6e192bfd8fd6849e1c1ea56efde6c6f4
Pulkit Agarwal <apulkit21@gmail.com>
2019-04-02 17:13:32 +0530

    u

--- script.bash
+++ script.bash
@@ -6,6 +6,7 @@ cd welcome_Puppet_Logs
 sudo chmod +x script.bash
 read -p "Enter slave no. ::"  Slave
 echo "$Slave" > host.txt
+cd /home/ec2-user
 echo "java -jar agent.jar -jnlpUrl http://3.84.239.54:8080/computer/$Slave/slave-agent.jnlp"  > main.sh
 sudo chmod +x main.sh
 ./main.sh
