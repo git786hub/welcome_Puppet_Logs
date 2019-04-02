47710cac3b253a21ca76b474a8edc530afab2ac5
Pulkit Agarwal <apulkit21@gmail.com>
2019-04-02 17:09:06 +0530

    update scipt.sh to script.bash

--- script.bash
+++ script.bash
@@ -1,8 +1,9 @@
 #!/bin/bash
 export Slave=0
 sudo yum install java -y
-sudo chmod +x /home/ec2-user/script.sh
 cd /home/ec2-user
+cd welcome_Puppet_Logs
+sudo chmod +x script.bash
 read -p "Enter slave no. ::"  Slave
 echo "$Slave" > host.txt
 echo "java -jar agent.jar -jnlpUrl http://3.84.239.54:8080/computer/$Slave/slave-agent.jnlp"  > main.sh
